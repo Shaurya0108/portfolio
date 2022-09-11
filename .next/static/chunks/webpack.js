170aaba0d4cb86134236ad52cf407af9966ad7a2
Shaurya <shauryadwivedi0108@gmail.com>
2022-09-11 10:00:28 -0500

    fixed package.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fb01e19c31d570f3"; }
+/******/ 		__webpack_require__.h = function() { return "2adda91f4eec62f4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
